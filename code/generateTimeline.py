07da160a805f502480901468d9e8ccb61ccbb064
Sahil Gandhi <sahilmgandhi@gmail.com>
2017-12-16 19:49:46 -0800

    Add csv parser for reviews

---
+++ code/generateTimeline.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import random, sys, string, csv, argparse
+
+parser=argparse.ArgumentParser(
+    description='''This script generates the HTML code for the timeline boxes''',
+    epilog="""Have fun!""")
+parser.add_argument('-f', default='movies.csv', dest='fileName', help='Name of the csv file. Default is movies.csv')
+args=parser.parse_args()
+
+outputFile = open('reviews.txt', 'w')
+currRating = 9
+counter = 0
+
+with open(args.fileName, 'r') as movies:
+    movieEntries = csv.reader(movies)
+    outputFile.write("<div id=\"9\">")
+    outputFile.write("<div id=\"ratingsBanner\"><h2>%d.00/10 - %d/10</h2>\n</div>" % (currRating, (currRating + 1)))
+    for row in movieEntries:
+        if int(float(row[0])) < currRating:
+            currRating = int(float(row[0]))
+            outputFile.write("</div>")
+            outputFile.write("<div id=\"%d\">" % (currRating))
+            outputFile.write("<div id=\"ratingsBanner\"><h2>%d.00/10 - %d.99/10</h2></div>" % (currRating, (currRating)))
+        if counter % 2 == 0:
+            outputFile.write("<div class=\"container left\">")
+        else:
+            outputFile.write("<div class=\"container right\">")
+        outputFile.write("<div class=\"timelineContent\">")
+        if row[1] == 'None':
+            outputFile.write("<p>No movies that are ranked in the %d's yet</p>" % (currRating))
+        else:    
+            outputFile.write("<h2>%.2f</h2>" % (float(row[0])))
+            outputFile.write("<p>%s</p>" % (str(row[1])))
+        outputFile.write("</div></div>")
+        counter += 1
+    outputFile.write("</div>")
